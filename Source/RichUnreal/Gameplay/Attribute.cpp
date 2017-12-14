5c717dcaa5b10b5e0082e46deff8c2f10777fc91
Evgeny Osmet <111osmet111@gmail.com>
2017-12-15 00:22:34 +0500

    Added Attribute

---
+++ Source/RichUnreal/Gameplay/Attribute.cpp
@@ -0,0 +1,88 @@
+// Copyright 2017 Evgeny Osmet.
+
+#include "Attribute.h"
+
+int32 FAttribute::GetMaxValue() const
+{
+	return MaxValue;
+}
+
+int32 FAttribute::GetMinValue() const
+{
+	return MinValue;
+}
+
+int32 FAttribute::GetValue()// const
+{
+	CheckBounds();
+	return Value;
+}
+
+void FAttribute::SetValue(int32 NewValue)
+{
+	int32 OldValue = Value;
+	Value = NewValue;
+	CheckBounds();
+	int32 DeltaValue = Value - OldValue;
+
+	if (DeltaValue != 0 || bValueChangedOnZeroDelta)
+	{
+		EValueSignState ValueSignState(EValueSignState::SAME_SIGN);
+		if (OldValue > 0 && Value <= 0)
+		{
+			ValueSignState = EValueSignState::BECAME_NEGATIVE;
+		}
+		else if (OldValue < 0 && Value >= 1)
+		{
+			ValueSignState = EValueSignState::BECAME_POSITIVE;
+		}
+		OnValueChanged.Broadcast(DeltaValue, ValueSignState);
+	}
+}
+
+
+void FAttribute::AddValue(int32 Value)
+{
+	SetValue(this->Value + Value);
+}
+
+int FAttribute::GetRemainingUpValue() const
+{
+	return MaxValue - Value;
+}
+
+int FAttribute::GetRemainingDownValue() const
+{
+	return Value - MinValue;
+}
+
+void FAttribute::ZeroOut()
+{
+	AddValue(-Value);
+}
+
+void FAttribute::CheckBounds()
+{
+	if (Value < MinValue)
+	{
+		if (!bCanGrowDown)
+		{
+			Value = MinValue;
+		}
+		else
+		{
+			MinValue = Value;
+		}
+	}
+	else if (Value > MaxValue)
+	{
+		if (!bCanGrowUp)
+		{
+			Value = MaxValue;
+		}
+		else
+		{
+			MaxValue = Value;
+		}
+	}
+}
---
+++ Source/RichUnreal/Gameplay/Attribute.h
@@ -0,0 +1,63 @@
+// Copyright 2017 Evgeny Osmet.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Attribute.generated.h"
+
+UENUM(/*BlueprintType*/)
+enum class EValueSignState : uint8
+{
+	BECAME_NEGATIVE,
+	BECAME_POSITIVE,
+	SAME_SIGN,
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnValueChangedSignature, int32, DeltaValue, EValueSignState, ValueSignState);
+
+USTRUCT(/*BlueprintType*/)
+struct FAttribute
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Properties)
+	bool bCanGrowUp = false;
+
+	UPROPERTY(EditAnywhere, Category = Properties)
+	bool bCanGrowDown = false;
+
+	UPROPERTY(EditAnywhere, Category = Properties)
+	bool bValueChangedOnZeroDelta = false;
+
+	//UPROPERTY(BlueprintAssignable, Category = Properties)
+	FOnValueChangedSignature OnValueChanged;
+
+	int32 GetMaxValue() const;
+
+	int32 GetMinValue() const;
+
+	int32 GetValue();// const;
+
+	void SetValue(int32 NewValue);
+
+	void AddValue(int32 Value);
+
+	int GetRemainingUpValue() const;
+	int GetRemainingDownValue() const;
+
+
+	void ZeroOut();
+
+private:
+	UPROPERTY(EditAnywhere, Category = Properties, meta = (AllowPrivateAccess = true/*, ClampMin = 0, ClampMax = 2147483647, UIMin = 0, UIMax = 2147483647*/))
+	int32 MaxValue = 2147483647;
+	UPROPERTY(EditAnywhere, Category = Properties, meta = (AllowPrivateAccess = true))
+	int32 MinValue = -2147483647 - 1;
+
+	UPROPERTY(EditAnywhere/*, BlueprintReadOnly*/, Category = Properties, meta = (AllowPrivateAccess = true))
+	int32 Value;
+
+	void CheckBounds();
+};
+
